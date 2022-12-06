a63503ad26b08805779ec2338c2391c1d4a63827
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-12-06 12:13:00 +0900

    feat: add creating empty file

--- bin/epger.js
+++ bin/epger.js
@@ -1,12 +1,11 @@
 #! /usr/bin/env node
-
 const fs = require('fs');
-const {qPush, qExec, c} = require('./fileHandler.js');
+const {qPush, qExec, c, getTemplate} = require('./fileHandler.js');
 
 console.log('');
-console.log('###');
-console.log('Easy rEACT: init');
-console.log('###');
+console.log('#======================#');
+console.log('# EPGER: Hello, World! #');
+console.log('#======================#');
 console.log('');
 
 const args = process.argv.slice(2);
@@ -17,50 +16,67 @@ const directoryQueue = ['.'];
 let isParams = false;
 for(const arg of args) {
   if(arg[0] === '/') {
+    // If you type '/' as the first letter,
+    // A directory is created.
     qPush(c.CREATE_DIR, `${directoryQueue.join('/')}/${arg.slice(1)}`);
     directoryQueue.push(arg.slice(1));
   } else if(arg === '..') {
-    if(directoryQueue.length === 1) continue;
+    // If you type '..' only,
+    // Move to the parent directory.
+    if(directoryQueue.length === 1) continue; // But if the current location is './', the command is ignored.
     directoryQueue.pop();
   } else {
+    /**
+     * Create Files
+     */
     const [filename, extension] = arg.split('.');
-
-    if(!extension) {
-      // No extension -> create empty file
-      console.log(`# creating empty file: ${directoryQueue.join('/')}/${arg}`);
-      fs.writeFileSync(`${directoryQueue.join('/')}/${arg}`, '');
-      continue;
-    }
-
+    const template = getTemplate(extension);
     const hasParams = arg[0] === '@';
-    if(hasParams) {
-      switch(extension) {
-        case 'js':
-        case 'jsx':
-          console.log(`# creating react script: ${directoryQueue.join('/')}/${arg.slice(1)}`);
-          fs.writeFileSync(`${directoryQueue.join('/')}/${arg.slice(1)}`, templates.REACT);
-          break;
-        default:
 
-      }
+    if(hasParams && template) {
+      // If you type '@' as tje first letter,
+      // You will receive the parameters until you encounter @ again.
+      // This only occurs when a template exists in EPGER.
     } else {
-      switch(extension) {
-        case 'js':
-        case 'jsx':
-          console.log(`# creating react script: ${directoryQueue.join('/')}/${arg}`);
-          // fs.writeFileSync(`${directoryQueue.join('/')}/${arg}`, templates.REACT);
+      // No parameters need.
+      // Only one parameter exists. - $0: filename(without extension)
+      if(template) {
 
-          // Read Template
-          const template = fs.readFileSync('./node_modules/eact/bin/templates/react-template.js').toString();
-
-          const reg = new RegExp(/\$0/, "g");
-          template.replace(reg, arg);
-          console.log(template.replace(reg, filename));
-          break;
-        default:
-
+      } else {
+        // If there is no template in EPGER,
+        // EPGER will create "Empty File" that don't need parameter
+        qPush(c.CREATE_EMPTY_FILE, directoryQueue.join('/'), arg);
       }
     }
+    
+    // if(hasParams) {
+    //   switch(extension) {
+    //     case 'js':
+    //     case 'jsx':
+    //       console.log(`# creating react script: ${directoryQueue.join('/')}/${arg.slice(1)}`);
+    //       fs.writeFileSync(`${directoryQueue.join('/')}/${arg.slice(1)}`, templates.REACT);
+    //       break;
+    //     default:
+
+    //   }
+    // } else {
+    //   switch(extension) {
+    //     case 'js':
+    //     case 'jsx':
+    //       console.log(`# creating react script: ${directoryQueue.join('/')}/${arg}`);
+    //       // fs.writeFileSync(`${directoryQueue.join('/')}/${arg}`, templates.REACT);
+
+    //       // Read Template
+    //       const template = fs.readFileSync('./node_modules/eact/bin/templates/react-template.js').toString();
+
+    //       const reg = new RegExp(/\$0/, "g");
+    //       template.replace(reg, arg);
+    //       console.log(template.replace(reg, filename));
+    //       break;
+    //     default:
+
+    //   }
+    // }
   }
 }
 
--- bin/fileHandler.js
+++ bin/fileHandler.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 
 const createDir = ([path, ]) => {
+  // TODO Exception
   if(fs.existsSync(path)) return;
 
   console.log(`# creating directory: ${path}`);
   fs.mkdirSync(path);
+}
+
+const createEmptyFile = ([path, filename]) => {
+  // TODO Exception
+  if(!fs.existsSync(path)) return;
+
+  console.log(`# creating empty file: ${path}/${filename}`);
+  fs.writeFileSync(`${path}/${filename}`, '');
 }
 
 const c = {
@@ -12,12 +21,20 @@ const c = {
   CREATE_EMPTY_FILE: 1,
 };
 
+const getTemplate = (extension) => {
+  if(!extension || !fs.existsSync(`./node_modules/eact/bin/templates/${extension}-template.js`)) return null;
+  return fs.readFileSync(`./node_modules/eact/bin/templates/${extension}-template.js`).toString();
+}
+
 const workQueue = [];
 
 const qPush = (work, ...params) => {
   switch(work) {
     case c.CREATE_DIR:
       workQueue.push(() => createDir(params));
+      break;
+    case c.CREATE_EMPTY_FILE:
+      workQueue.push(() => createEmptyFile(params));
       break;
   }
 }
@@ -30,4 +47,5 @@ module.exports = {
   qPush,
   qExec,
   c,
+  getTemplate,
 };
