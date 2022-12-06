c31bc9da6f5857abad043210c5227d4f4c11c7c1
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2022-12-06 13:43:48 +0900

    feat: add creating template file with params

--- bin/epger.js
+++ bin/epger.js
@@ -14,7 +14,47 @@ console.log(args);
 const directoryQueue = ['.'];
 
 let isParams = false;
+const workingFile = {
+  filename: '',
+  extension: '',
+  template: '',
+  hasFilename: false,
+
+  init: function(filename = '', extension = '', template = '') {
+    this.filename = filename;
+    this.extension = extension;
+    this.template = template;
+    this.hasFilename = false;
+  },
+  refresh: function() {
+    this.init();
+  },
+};
 for(const arg of args) {
+  if(isParams) {
+    // After type "@*",
+    if(arg === '@') {
+      // If you type "@" only,
+      // Ends the process of receiving parameters.
+      if(!workingFile.hasFilename) {
+        // If you didn't enter $0,
+        // Convert all $0 to filename.
+        workingFile.template = workingFile.template.replace(new RegExp('!0', "g"), workingFile.filename);    
+      }
+
+      qPush(c.CREATE_TEMPLATE_FILE, directoryQueue.join('/'), workingFile.filename, workingFile.extension, workingFile.template);
+
+      continue;
+    }
+
+    // Parameters must be entered in the following format: parameterKey=parameterValue
+    const [key, value] = arg.split('=');
+    if(key === '0') workingFile.hasFilename = true;
+    workingFile.template = workingFile.template.replace(new RegExp(`!${key}`, "g"), value);
+
+    continue;
+  }
+
   if(arg[0] === '/') {
     // If you type '/' as the first letter,
     // A directory is created.
@@ -38,12 +78,14 @@ for(const arg of args) {
       // If you type '@' as tje first letter,
       // You will receive the parameters until you encounter @ again.
       // This only occurs when a template exists in EPGER.
+      isParams = true;
+      workingFile.init(filename.slice(1), extension, template);
     } else {
       // No parameters need.
       // Only one parameter exists. - $0: filename(without extension)
       if(template) {
         // Convert all $0 to filename.
-        const content = template.replace(new RegExp(/\$0/, "g"), filename);
+        const content = template.replace(new RegExp('!0', "g"), filename);
         qPush(c.CREATE_TEMPLATE_FILE, directoryQueue.join('/'), filename, extension, content);
       } else {
         // If there is no template in EPGER,
@@ -51,35 +93,6 @@ for(const arg of args) {
         qPush(c.CREATE_EMPTY_FILE, directoryQueue.join('/'), arg);
       }
     }
-    
-    // if(hasParams) {
-    //   switch(extension) {
-    //     case 'js':
-    //     case 'jsx':
-    //       console.log(`# creating react script: ${directoryQueue.join('/')}/${arg.slice(1)}`);
-    //       fs.writeFileSync(`${directoryQueue.join('/')}/${arg.slice(1)}`, templates.REACT);
-    //       break;
-    //     default:
-
-    //   }
-    // } else {
-    //   switch(extension) {
-    //     case 'js':
-    //     case 'jsx':
-    //       console.log(`# creating react script: ${directoryQueue.join('/')}/${arg}`);
-    //       // fs.writeFileSync(`${directoryQueue.join('/')}/${arg}`, templates.REACT);
-
-    //       // Read Template
-    //       const template = fs.readFileSync('./node_modules/eact/bin/templates/react-template.js').toString();
-
-    //       const reg = new RegExp(/\$0/, "g");
-    //       template.replace(reg, arg);
-    //       console.log(template.replace(reg, filename));
-    //       break;
-    //     default:
-
-    //   }
-    // }
   }
 }
 
--- bin/templates.js
+++ bin/templates.js
@@ -1,28 +1,28 @@
 const js =
 `import React from 'react';
 
-const $0 = () => {};
+const !0 = () => {};
 
-export default $0;`;
+export default !0;`;
 
 const jsx = 
 `import React from 'react';
 
-const $0 = () => {};
+const !0 = () => {};
 
-export default $0;`;
+export default !0;`;
 
 const css =
 ``;
 
 const html =
 `<!DOCTYPE html>
-<html lang="$1">
+<html lang="!lang">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>$0</title>
+  <title>!0</title>
 </head>
 <body>
   
